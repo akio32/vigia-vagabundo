33fecfa32c5f909f1c6bb25d3134c8d7890a6f22
akio32 <oliver.akio@gmail.com>
2023-03-22 17:25:25 -0300

    scripts py

--- main_ingest.py
+++ main_ingest.py
@@ -1,7 +1,7 @@
 from api_utils import call_api
 
 def main():
-    url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes?ano=2022&siglaTipo=PL&itens=10'
+    url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes/2313792'
     response = call_api(url)
     print(response)
 
