1be5cbe58ad59f0ab2e26e26ee695fc73abc39d4
Misteryoh <oliver.akio@gmail.com>
2023-03-30 23:26:28 -0300

    ajuste metodo, gerando CSV resultado

--- main_ingest.py
+++ main_ingest.py
@@ -1,9 +1,12 @@
-from api_utils import call_api
+import api_utils as api
+import csv
 
 def main():
-    url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes?ano=2022&siglaTipo=PL&itens=10'
-    response = call_api(url)
-    print(response)
+    # url = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes?ano=2022&siglaTipo=PL&itens=10'
+    # response = call_api(url)
+    # print(response)
+
+    api.consulta_propostas()
 
 if __name__ == '__main__':
     main()
